17c874de05af5e853d3df5a7c0f512ee9741834e
Валентин Ключник <58791598+valkl777@users.noreply.github.com>
2025-03-23 21:55:56 +0300

    feat: функция generateReport
    + Ф отчета число в списке >= 0

--- code.py
+++ code.py
@@ -11,4 +11,34 @@ def round_value(x):
     return round(x, 2)
 
 
+def generateReport(x_list):
+    """
+    Generate report
+    
+    Args:
+        x_list (list): list of x value
+
+    Returns:
+        list: result
+    """
+    report = []
+    
+    for i in range(0, len(x_list)):
+        x = x_list[i]
+        
+        result = False
+        if x >= 0:
+            result = True
+        report.append(result)
+    
+    return report
+
+
 print(str(round_value(100.2345)))
+
+x_list = [
+    -1,
+    1
+]
+report = generateReport(x_list)
+print(report)
